e02caac365272a706ab1158389f90eb1cd1a3b9c
Alifkm <alifkahfi@gmail.com>
2024-10-22 22:51:03 +0700

    feat: first commit

---
+++ GenericCSharpChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericCSharpChallenge
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailTask emailTask = new EmailTask()
+            {
+                Message = "Hello, ini messagenya nih",
+                Recipient = "Gue@gmail.com"
+            };
+
+            ReportTask reportTask = new ReportTask()
+            {
+                ReportName = "Gue"
+            };
+
+            var emailProcessor = new TaskProcessor<EmailTask, string>(emailTask);
+            var reportProcessor = new TaskProcessor<ReportTask, string>(reportTask);
+
+            Console.WriteLine(emailProcessor.Execute());
+            Console.WriteLine(reportProcessor.Execute());
+        }
+    }
+
+}
